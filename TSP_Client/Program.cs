9ed607db1b96d1874fd4e51b9bc5d53890a14874
Genadij1 <bgennadij265@icloud.com>
2024-07-22 19:00:44 +0300

    ex_done

--- TSP_Client/Program.cs
+++ TSP_Client/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client
+{
+    static async Task Main()
+    {
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+        TcpClient client = new TcpClient();
+
+        try
+        {
+            await client.ConnectAsync(ip, port);
+            NetworkStream stream = client.GetStream();
+            IPEndPoint serverEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+            string serverIP = serverEndPoint.Address.ToString();
+
+            string clientMessage = "Hello server!";
+            byte[] messageBytes = Encoding.ASCII.GetBytes(clientMessage);
+            await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+
+            byte[] buffer = new byte[1024];
+            int receivedLength = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string serverResponse = Encoding.ASCII.GetString(buffer, 0, receivedLength);
+
+            Console.WriteLine($"о {DateTime.Now:HH:mm} від [{serverIP}]  {serverResponse}");
+
+            client.Close();
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
